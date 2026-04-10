c84da59a3eebc948fc1bfcf80378a22ac4307591
23wh1a0513 <23wh1a0513@bvrithyderabad.edu.in>
2026-04-10 20:17:34 +0530

    [FEATURE] added /tasks route for task list

--- src/App.js
+++ src/App.js
@@ -1,32 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import {BrowersRouter,Routes} from 'react-router-dom';
-import studentList from './pages/StudentList';
-import editStudent from './pages/editStudent';
-import addstudent from './pages/addstudent'; 
+import QuestionPage from './pages/QuestionPage';
 
 function App() {
   return (
-    <div>
-      <Home />
-
-      {}
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/tasks" element={<QuestionPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
-//Check status:
-git status
-
-Stage your modified files:
-git add src/App.js
-
-Or stage all changes:
-git add .
-
-Commit:
-git commit -m "Describe your changes"
-
-Push to the current branch:
-git push
