b1ea3e1a6b995b01af476eb9009999c881468f5d
23wh1a0513 <23wh1a0513@bvrithyderabad.edu.in>
2026-04-10 13:10:20 +0530

    INIT

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import Home from './pages/Home';
+import {BrowersRouter,Routes} from 'react-router-dom';
+import studentList from './pages/StudentList';
+import editStudent from './pages/editStudent';
+import addstudent from './pages/addstudent'; 
 
 function App() {
   return (
     <div>
       <Home />
 
-      {/* TODO: Replace this placeholder with Routes */}
+      {}
     </div>
   );
 }
 
 export default App;
+//Check status:
+git status
+
+Stage your modified files:
+git add src/App.js
+
+Or stage all changes:
+git add .
+
+Commit:
+git commit -m "Describe your changes"
+
+Push to the current branch:
+git push
